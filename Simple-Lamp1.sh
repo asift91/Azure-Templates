2f2e8bc9d24a6b0f2edd50618f2e6b8ffd2af30f
asif <asift@italentdigital.com>
2020-02-05 11:49:49 +0530

    added Simple lamp1.sh modified file

--- Simple-Lamp1.sh
+++ Simple-Lamp1.sh
@@ -9,11 +9,12 @@ sudo ufw allow 3306
 dbpass=Q!w2e3r4
 
 export DEBIAN_FRONTEND=noninteractive
-echo mysql-server-5.6 mysql-server/root_password password $dbpass | debconf-set-selections
-echo mysql-server-5.6 mysql-server/root_password_again password $dbpass | debconf-set-selections
+echo "mysql-server-5.6 mysql-server/root_password password" $dbpass | sudo debconf-set-selections
+echo "mysql-server-5.6 mysql-server/root_password_again password" $dbpass | sudo debconf-set-selections
+sudo apt-get -y install mysql-server-5.6
 
 # install the LAMP stack
-apt-get -y install apache2 mysql-server php5 php5-mysql
+sudo apt-get -y install apache2 php5
 
 # restart Apache
 sudo service apache2 restart
