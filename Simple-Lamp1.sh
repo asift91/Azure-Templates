8f3a3dfe0b463476c05a18e9def4c265aceeb940
asif <asift@italentdigital.com>
2020-02-05 16:45:23 +0530

    added Simple lamp1.sh file modified

--- Simple-Lamp1.sh
+++ Simple-Lamp1.sh
@@ -2,7 +2,7 @@
 sudo apt-get -y update
 
 # set up a silent install of MySQL
-dbpass=Q!w2e3r4
+dbpass=$1
 
 export DEBIAN_FRONTEND=noninteractive
 echo "mysql-server-5.7 mysql-server/root_password password" $dbpass | sudo debconf-set-selections
@@ -10,8 +10,9 @@ echo "mysql-server-5.7 mysql-server/root_password_again password" $dbpass | sudo
 sudo apt-get -y install mysql-server-5.7
 
 # install the LAMP stack
-sudo apt-get -y install apache2 php7.2
+sudo apt-get -y install apache2 php7
 
 # restart Apache
 sudo service apache2 restart
+
 
