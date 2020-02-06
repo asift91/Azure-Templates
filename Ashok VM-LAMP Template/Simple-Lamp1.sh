f43d84ef7b4d01dcd6501f3e39cf4ae78cedd1c3
asif <asift@italentdigital.com>
2020-02-06 16:53:17 +0530

    json file modified by Ashok on our first meet (06th feb 2020) and shell script file

---
+++ Ashok VM-LAMP Template/Simple-Lamp1.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+sudo apt-get -y update
+
+# set up a silent install of MySQL
+dbpass=Q!w2e3r4
+
+export DEBIAN_FRONTEND=noninteractive
+echo "mysql-server-5.7 mysql-server/root_password password" $dbpass | sudo debconf-set-selections
+echo "mysql-server-5.7 mysql-server/root_password_again password" $dbpass | sudo debconf-set-selections
+sudo apt-get -y install mysql-server-5.7
+
+# install the LAMP stack
+sudo apt-get -y install apache2 php7
+
+# restart Apache
+sudo service apache2 restart
